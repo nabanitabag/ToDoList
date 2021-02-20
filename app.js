aedb8403f6204d695e0e891ee55708f02f78af9d
Nabanita <ashley.makruz@gmail.com>
2021-02-20 18:44:53 +0530

    add days functionality

--- app.js
+++ app.js
@@ -2,16 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function (req,res) {
 
   var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if(today.getDay()===6 || today.getDay() === 0){
-    res.send("Yay, it's the weekend.")
+  switch(currentDay)
+  {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break
+    default:
+        console.log("Current day is "+ currentDay);
   }
-  else{
-  res.send("It's a workday.");
-  }
+  res.render("list", {kindOfDay: day});
 });
 
 app.listen(process.env.PORT || 3000,function() {
