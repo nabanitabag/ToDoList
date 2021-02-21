04e05917adcb92bf7794d95447ea096b2bf9009d
Nabanita <ashley.makruz@gmail.com>
2021-02-21 06:11:43 +0530

    add functioality using EJS

--- app.js
+++ app.js
@@ -2,41 +2,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+let items =["Grow food", "Cook well", "Eat it slow"];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/", function (req,res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let day = today.toLocaleDateString("en-US", options);
+  //  var currentDay = today.getDay();
+  // switch(currentDay)
+  // {
+  //   case 0:
+  //       day = "Sunday";
+  //       break;
+  //   default:
+  //       console.log("Current day is "+ currentDay);
+  // }
+  res.render("list", {kindOfDay: day, newListItems: items});
+});
 
-  switch(currentDay)
-  {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-        break
-    default:
-        console.log("Current day is "+ currentDay);
-  }
-  res.render("list", {kindOfDay: day});
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000,function() {
