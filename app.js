4c53343ad8027e930b62a1ff3812f7c416a00d69
Nabanita Bag <ashley.makruz@gmail.com>
2021-02-24 01:37:53 +0530

    Delete list item operation using MongoDB features

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-//listmaker-made with love by Nabanita
-
+//listmaker
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 
 const app = express();
 
-const items =["Grow food", "Cook well", "Eat it slow"];
+// const items =["Grow food", "Cook well", "Eat it slow"];
 const workItems =[];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
+
+const itemSchema = {
+  name: String
+};
+
+const Item = mongoose.model("item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your ToDo list!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new line."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function (req,res) {
   //  var currentDay = today.getDay();
@@ -25,30 +47,64 @@ app.get("/", function (req,res) {
   //       console.log("Current day is "+ currentDay);
   // }
   const day = date.getDate();
-  res.render("list", {ListTitle: day, newListItems: items});
+
+  Item.find({}, function(err, foundItems) {
+
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log("success");
+        }
+      });
+      res.redirect("/");
+    } else {
+    res.render("list", {ListTitle: day, newListItems: foundItems});
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
   if(req.body.list === "Work"){
-    workItems.push(item);
     res.redirect("/work");
   }
   else{
-    items.push(item);
     res.redirect("/");
   }
 });
 
-app.get("/work",function (req, res) {
-    res.render("list", {ListTitle: "Work List", newListItems: workItems})
-});
+app.post("/delete", function (req, res) {
 
-app.post("/work",function (req, res) {
-    const item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (err) {
+      console.log(err);
+    }else {
+      console.log("Success in deleting");
+      res.redirect("/");
+    }
+  });
+
 });
+// app.get("/work",function (req, res) {
+//     res.render("list", {ListTitle: "Work List", newListItems: workItems})
+// });
+//
+// app.post("/work",function (req, res) {
+//     const item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
 
 app.get("/about", function(req, res){
   res.render("about");
